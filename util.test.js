acfb30eca319110eccf9b2ebd195742fa9d1a006
e.v.e.r.e.s.tt.1551 <e.v.e.r.e.s.tt.1551@gmail.com>
2019-10-30 11:34:36 +0300

    e2e tests

--- util.test.js
+++ util.test.js
@@ -1,4 +1,5 @@
 const { generateText, checkAndGenerate } = require('./util');
+const puppeteer = require('puppeteer');
 
 //unit tests
 test('should output age and name', () =>{
@@ -24,3 +25,29 @@ test('should generate a valid text output', () =>{
     const text = checkAndGenerate('Max', 29);
     expect(text).toBe('Max (29 years old)');
 });
+
+//e2e test
+
+test('should click around', async () =>{
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 80,
+        args: ['--window-size=1920,1080']
+    });
+    const page = await browser.newPage();
+    await page.goto(
+        'file:///D:/projects/js-testing-introduction/index.html'
+    );
+    await page.click('input#name');
+    await page.type('input#name', 'Anna');
+
+    await page.click('input#age');
+    await page.type('input#age', '28');
+
+    await page.click('#btnAddUser');
+
+    const finalText = await page.$eval('.user-item', el => el.textContent);
+    expect(finalText).toBe('Anna (28 years old)');
+
+}, 10000); /* add 10 seconds because jest default test time is 
+                5 seconds and our test takes more than 5 seconds */
